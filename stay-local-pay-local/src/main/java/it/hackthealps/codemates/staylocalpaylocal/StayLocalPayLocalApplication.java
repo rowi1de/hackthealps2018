79187bfe55c253029923a02a651eb369f3eb5909
rowi1de <robert@scalable.capital>
2018-09-21 20:01:12 +0200

    Empty Spring Project -- start hacking!

---
+++ stay-local-pay-local/src/main/java/it/hackthealps/codemates/staylocalpaylocal/StayLocalPayLocalApplication.java
@@ -0,0 +1,12 @@
+package it.hackthealps.codemates.staylocalpaylocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayLocalPayLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayLocalPayLocalApplication.class, args);
+	}
+}
