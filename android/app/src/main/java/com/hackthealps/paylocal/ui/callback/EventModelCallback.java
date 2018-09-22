57e5808ffc14fd2baa0dfbdcfd5d2c431184504b
rowi1de <robert@scalable.capital>
2018-09-22 16:19:39 +0200

    WIP

---
+++ android/app/src/main/java/com/hackthealps/paylocal/ui/callback/EventModelCallback.java
@@ -0,0 +1,7 @@
+package com.hackthealps.paylocal.ui.callback;
+
+import com.hackthealps.paylocal.model.EventModel;
+
+public interface EventModelCallback {
+    void onClick(EventModel eventModel);
+}
